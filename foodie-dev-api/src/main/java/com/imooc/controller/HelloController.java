cfc363d57181ef1710c930d956fa8d86240ff542
JacksonChao <17892709783@qq.com>
2020-08-02 21:16:15 +0800

    天天吃货项目
    Signed-off-by: JacksonChao <17892709783@qq.com>

---
+++ foodie-dev-api/src/main/java/com/imooc/controller/HelloController.java
@@ -0,0 +1,15 @@
+package com.imooc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.annotations.ApiIgnore;
+
+@RestController
+@ApiIgnore
+public class HelloController {
+     @GetMapping("hello")
+     public Object getObj(){
+         return "hell,controller";
+     }
+}
